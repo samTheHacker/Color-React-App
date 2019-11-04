17b7704d9d1ed42aad126cfe2618a8b2c9648bd1
Sam <mobilvodoo@googlemail.com>
2019-11-04 00:07:55 +0000

    Updates

---
+++ precache-manifest.c79cda7d254ab74f4cbb07dee80f551d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc98de7fb728f6482b315d567ddbf714",
+    "url": "/Color-React-App/index.html"
+  },
+  {
+    "revision": "0529e8570a638196edbf",
+    "url": "/Color-React-App/static/css/2.292c8731.chunk.css"
+  },
+  {
+    "revision": "60298eb1c73748e3695e",
+    "url": "/Color-React-App/static/css/main.d30f7c7a.chunk.css"
+  },
+  {
+    "revision": "0529e8570a638196edbf",
+    "url": "/Color-React-App/static/js/2.b37d539b.chunk.js"
+  },
+  {
+    "revision": "60298eb1c73748e3695e",
+    "url": "/Color-React-App/static/js/main.6ab0ae03.chunk.js"
+  },
+  {
+    "revision": "cc28de9db125e5a88222",
+    "url": "/Color-React-App/static/js/runtime-main.ec8f8513.js"
+  },
+  {
+    "revision": "66ab7a4954a3301af87d4329bc3f124e",
+    "url": "/Color-React-App/static/media/bg.66ab7a49.svg"
+  }
+]);
